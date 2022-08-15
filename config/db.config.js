448299ad0f71eaeab147c560322364ec90c3d96f
phamsongluong <psluong952@gmail.com>
2022-08-15 13:50:58 +0700

    Config database for pushing into heroku

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "remitano",
+    HOST: "us-cdbr-east-06.cleardb.net",
+    USER: "beb413495237c1",
+    PASSWORD: "e9167e70",
+    DB: "heroku_62c747fbe40fe6d",
     dialect: "mysql",
     pool: {
       max: 5,
