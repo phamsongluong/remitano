c42138bf90fe798e36535103bda172c3779ef7f6
phamsongluong <psluong952@gmail.com>
2022-08-19 21:38:03 +0700

    Fix database config

--- config/db.config.js
+++ config/db.config.js
@@ -3,12 +3,6 @@ module.exports = {
     USER: "beb413495237c1",
     PASSWORD: "e9167e70",
     DB: "heroku_62c747fbe40fe6d",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+    dialect: "mysql"
   };
   
